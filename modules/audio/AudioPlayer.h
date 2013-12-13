4f9683484a59b5185307ed817176db5273677c17
Contreras Pinilla, Ignacio <ignacio.contreras@i2cat.net>
2013-12-13 11:01:03 +0100

    Add audio API skeleton

---
+++ modules/audio/AudioPlayer.h
@@ -0,0 +1,41 @@
+/*
+ *  thin render - Mobile render engine based on OpenGL ES 2.0
+ *  Copyright (C) 2013  Fundació i2CAT, Internet i Innovació digital a Catalunya
+ *
+ *  This file is part of thin render.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Author:         Ignacio Contreras Pinilla <ignacio.contreras@i2cat.net>
+ */
+
+#ifndef AUDIO_PLAYER_H
+#define AUDIO_PLAYER_H
+
+class AudioPlayer {
+
+public:
+    AudioPlayer();
+    ~AudioPlayer();
+
+	void setSource(std::string fileName);
+	void play();
+	void pause();
+	void stop();
+
+private:
+
+};
+
+#endif
